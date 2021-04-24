036634aeb9aef0b81840b1d04f288fa5ef2030e9
unknown <it.borki@gmail.com>
2021-04-24 16:42:15 +0300

    init: start project Social network on React

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://api.freelogodesign.org/assets/thumb/logo/3343895_400.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Massages</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Music</a>
+      </div>
+      <div>
+        <a>Setings</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
