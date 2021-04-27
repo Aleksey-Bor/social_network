8c99992446fc02bca1cd065a7014cf0fce182aec
aliaksei barkouski <it.borki@gmail.com>
2021-04-27 13:34:58 +0300

    feat: register routing for navbar links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <nav className={style.nav}>
       <div className={`${style.item} ${style.active}`}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={style.item}>
-        <a>Massages</a>
+        <a href="/dialogs">Massages</a>
       </div>
       <div className={style.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={style.item}>
-        <a>Music</a>
+        <a href="/music">Music</a>
       </div>
       <div className={style.item}>
-        <a>setings</a>
+        <a href="/settings">Setings</a>
       </div>
     </nav>
   );
