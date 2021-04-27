ff8ec8444fd113ddec3133c0a3ae3e243aadf512
aliaksei barkouski <it.borki@gmail.com>
2021-04-27 17:05:51 +0300

    refactor: start filling the component with dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,30 @@ import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>Dialogs</div>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Alex
+                </div>
+                <div className={style.dialog}>
+                    Nastya
+                </div>
+                <div className={style.dialog}>
+                    Marsya
+                </div>
+                <div className={style.dialog}>
+                    Bill
+                </div>
+                <div className={style.dialog}>
+                    Steve
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi!</div>
+                <div className={style.message}>How do you like programming?</div>
+                <div className={style.message}>I love learning React with the IT-KamaSutra.</div>
+            </div>
+        </div>
     );
 }
 
