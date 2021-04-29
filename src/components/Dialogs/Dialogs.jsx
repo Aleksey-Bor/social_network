0d58ff7fae93ea76cb760d0b7ce8dd51a1c325f5
aliaksei barkouski <it.borki@gmail.com>
2021-04-29 19:50:21 +0300

    refactor: Prepare objects with data for their introduction into business logic(BLL)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,21 +18,41 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsDate = [
+        {id: 1, name: 'Alex'},
+        {id: 2, name: 'Nastya'},
+        {id: 3, name: 'Marsya'},
+        {id: 4, name: 'Steve'},
+        {id: 5, name: 'Bill'},
+        {id: 6, name: 'Elon'}
+    ];
+
+    let messagesDate = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How do you like programming?'},
+        {id: 3, message: 'I love learning React with the IT-KamaSutra.'},
+        {id: 4, message: 'Yo!'},
+        {id: 5, message: 'Yo!'},
+        {id: 6, message: 'Yo!'}
+    ];
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <DialogItem name="Alex" id="1"/>
-                <DialogItem name="Nastya" id="2"/>
-                <DialogItem name="Marsya" id="3"/>
-                <DialogItem name="Steve" id="4"/>
-                <DialogItem name="Bill" id="5"/>
-                <DialogItem name="Elon" id="6"/>
+                <DialogItem name={dialogsDate[0].name} id={dialogsDate[0].id}/>
+                <DialogItem name={dialogsDate[1].name} id={dialogsDate[1].id}/>
+                <DialogItem name={dialogsDate[2].name} id={dialogsDate[2].id}/>
+                <DialogItem name={dialogsDate[3].name} id={dialogsDate[3].id}/>
+                <DialogItem name={dialogsDate[4].name} id={dialogsDate[4].id}/>
+                <DialogItem name={dialogsDate[5].name} id={dialogsDate[5].id}/>
             </div>
             <div className={style.messages}>
-                <Message message="Hi"/>
-                <Message message="How do you like programming?"/>
-                <Message message="I love learning React with the IT-KamaSutra."/>
-                <Message message="Yo!"/>
+                <Message message={messagesDate[0].message} id={messagesDate[0].id}/>
+                <Message message={messagesDate[1].message} id={messagesDate[1].id}/>
+                <Message message={messagesDate[2].message} id={messagesDate[2].id}/>
+                <Message message={messagesDate[3].message} id={messagesDate[3].id}/>
+                <Message message={messagesDate[4].message} id={messagesDate[4].id}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postsDate = [
+        {id: 1, message: 'Hi, how  are you?', likesCount: 12},
+        {id: 2, message: 'It\'s  my first post',  likesCount: 76},
+        {id: 3, message: 'Hello Nastya how  are you?',  likesCount: 22},
+    ];
+
     return (
         <div className={style.postsBlock}>
             <h3>My posts</h3>
@@ -15,13 +22,9 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={style.posts}>
-                <Post message="Hi, how  are you?" likeCount="15"/>
-                <Post message="It's  my first post" likeCount="155"/>
-                <Post message="Hello Nastya how  are you?" likeCount="76"/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={postsDate[0].message} likeCount={postsDate[0].likesCount} id={postsDate[0].id}/>
+                <Post message={postsDate[1].message} likeCount={postsDate[1].likesCount} id={postsDate[1].id}/>
+                <Post message={postsDate[2].message} likeCount={postsDate[2].likesCount} id={postsDate[2].id}/>
             </div>
         </div>
     );
