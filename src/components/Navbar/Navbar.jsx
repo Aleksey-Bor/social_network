64b576f4533dde839862a3b96eca9ec9c6b44a9b
aliaksei barkouski <it.borki@gmail.com>
2021-05-23 16:37:30 +0300

    refactor: Rewrite the User's functional component to a class one

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,17 +8,20 @@ const Navbar = () => {
       <div className={style.item}>
         <NavLink to="/profile" activeClassName={style.activeLink}>Profile</NavLink>
       </div>
-      <div className={style.item}>
-        <NavLink to="/dialogs" activeClassName={style.activeLink}>Massages</NavLink>
+        <div className={style.item}>
+            <NavLink to="/dialogs" activeClassName={style.activeLink}>Massages</NavLink>
+        </div>
+        <div className={style.item}>
+            <NavLink to="/users" activeClassName={style.activeLink}>Users</NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/news" activeClassName={style.activeLink}>News</NavLink>
       </div>
-      <div className={style.item}>
-        <NavLink to="/news" activeClassName={style.activeLink}>News</NavLink>
+        <div className={style.item}>
+          <NavLink to="/music" activeClassName={style.activeLink}>Music</NavLink>
       </div>
-      <div className={style.item}>
-        <NavLink to="/music" activeClassName={style.activeLink}>Music</NavLink>
-      </div>
-      <div className={style.item}>
-        <NavLink to="/settings" activeClassName={style.activeLink}>Setings</NavLink>
+        <div className={style.item}>
+          <NavLink to="/settings" activeClassName={style.activeLink}>Setings</NavLink>
       </div>
     </nav>
   );
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,19 +3,18 @@ import style from "./Users.module.css";
 import * as axios from "axios";
 import userPhoto from "./../../../src/assets/images/user.png";
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
-            });
-        }
-    };
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
 
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null
@@ -25,12 +24,12 @@ const Users = (props) => {
                     <div>
                         {
                             u.followed
-                            ? <button onClick={() => {props.unfoll(u.id)}}>Unfollow</button>
-                            : <button onClick={() => {props.foll(u.id)}}>Follow</button>
+                                ? <button onClick={() => {this.props.unfoll(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {this.props.foll(u.id)}}>Follow</button>
                         }
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -40,9 +39,10 @@ const Users = (props) => {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
-};
+                </div>)
+            }
+        </div>
+    }
+}
 
 export default Users;
