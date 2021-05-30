588a12d02a9cdc5529f97b2eaa12e071e101c5a1
aliaksei barkouski <it.borki@gmail.com>
2021-05-30 15:45:53 +0300

    feat: Implement displaying the avatar and full name of the user on the profile page after clicking on the avatar of his avatar on the users page

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import { connect } from "react-redux";
 import Profile from "./Profile";
 import * as axios from "axios";
 import { setUserProfile } from "../../Redux/profileReducer";
+import { withRouter } from "react-router";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -26,4 +31,8 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let WithUrlDadeContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(
+  WithUrlDadeContainerComponent
+);
