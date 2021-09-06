e9e2ce01083bdb7abd1075f3d793ba2aadccd9a6
aliaksei barkouski <it.borki@gmail.com>
2021-09-06 19:10:15 +0300

    Apply destructuring to pass props in the ProfileInfo component

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,20 +3,20 @@ import Preloader from "../../common/preloader/preloader";
 import style from "./ProfileInfo.module.css";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
-const ProfileInfo = (props) => {
-  if (!props.profile) {
+const ProfileInfo = ({profile, status, updateStatus}) => {
+  if (!profile) {
     return <Preloader />;
   }
 
   return (
     <div>
-      {/*       <div>
-        <img src="https://image.freepik.com/free-photo/idyllic-beach-scenery-tropical-paradise-mauritius-island_287743-1231.jpg" />
-      </div> */}
       <div className={style.descriptionBlock}>
-        <img src={props.profile.photos.large} alt="avatar" />
-        <p>{props.profile.fullName}</p>
-        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
+        <img src={profile.photos.large} alt="avatar" />
+        <p>{profile.fullName}</p>
+        <ProfileStatusWithHooks
+          status={status}
+          updateStatus={updateStatus}
+        />
       </div>
     </div>
   );
