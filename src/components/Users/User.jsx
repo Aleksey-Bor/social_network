436e3d7cd6f0e6612cb54af3ccff39a82671b5d4
aliaksei barkouski <it.borki@gmail.com>
2021-09-06 19:12:48 +0300

    Remove unused imports in the User component

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import style from "./Users.module.css";
 import userPhoto from "../../assets/images/user_unisex.png";
 import { NavLink } from "react-router-dom";
-import Paginator from "../common/Paginator/Paginator";
 
 let User = ({ user, followingInProgress, unFollowToUser, followToUser }) => {
   return (
