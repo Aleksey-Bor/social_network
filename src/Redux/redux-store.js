5732c96c2a3a7c2b158ec7d78fc8f8382026afe2
aliaksei barkouski <it.borki@gmail.com>
2021-09-07 16:05:04 +0300

    test: To plug chromeextenthion ReduxDevTools

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore, compose} from "redux";
 import dialogsReducer from "./dialogsReducer";
 import profileReducer from "./profileReducer";
 import sidebarReducer from "./sidebarReducer";
@@ -18,6 +18,12 @@ let reducers = combineReducers({
   app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
+
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+window.__store__ = store;
 export default store;
