b6c75e31bf945a4bbc5f110f59b089a4795d8955
Aliaksey <it.borki@gmail.com>
2022-01-10 15:04:27 +0300

    Add TypeScript to dialogsReducer

--- src/Redux/dialogsReducer.ts
+++ src/Redux/dialogsReducer.ts
@@ -1,5 +1,8 @@
 const SEND_MESSAGE = "alex_samurai_network/dialogs/SEND-MESSAGE";
 
+type MessagesType = { id: number; message: string };
+type DialogsType = { id: number; name: string };
+
 let initialState = {
   messagesData: [
     { id: 1, message: "Hi!" },
@@ -8,7 +11,7 @@ let initialState = {
     { id: 4, message: "Yo!" },
     { id: 5, message: "Yo!" },
     { id: 6, message: "Yo!" },
-  ],
+  ] as Array<MessagesType>,
   dialogsData: [
     { id: 1, name: "Alex" },
     { id: 2, name: "Nastya" },
@@ -16,10 +19,12 @@ let initialState = {
     { id: 4, name: "Steve" },
     { id: 5, name: "Bill" },
     { id: 6, name: "Elon" },
-  ],
+  ] as Array<DialogsType>,
 };
 
-const dialogsReducer = (state = initialState, action) => {
+export type InitialStateType = typeof initialState;
+
+const dialogsReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case SEND_MESSAGE: {
       let body = action.newMessageBody;
@@ -33,7 +38,12 @@ const dialogsReducer = (state = initialState, action) => {
   }
 };
 
-export const sendMessageActionCreator = (newMessageBody) => ({
+export type SendMessageActionCreatorType = {
+  type: typeof SEND_MESSAGE;
+  newMessageBody: string;
+};
+
+export const sendMessageActionCreator = (newMessageBody: string) => ({
   type: SEND_MESSAGE,
   newMessageBody,
 });
